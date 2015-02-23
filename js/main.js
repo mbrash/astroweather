ad16bd1748aa10c23d83599ef4259f43bbf35006
Michael Brash <mbrash123@gmail.com>
2015-02-23 11:02:52 -0500

    Project Setup

---
+++ js/main.js
@@ -0,0 +1,2 @@
+// main.js
+console.log("Main Here")
---
+++ js/server.js
@@ -0,0 +1,9 @@
+// server.js
+// get api data
+console.log("Server Here")
+
+// Need a location
+// Send Request
+// Get Request
+// Parse Request 
+// Do Something with this data..
