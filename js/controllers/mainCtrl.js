3e47d3ece5b9c32c59908df730d30ba1fc60211b
Michael Brash <mbrash123@gmail.com>
2015-03-04 14:33:39 -0500

    Generate status function added and data binded succesfully

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,119 +1,62 @@
-app.controller('MainCtrl', ['$http', function($http) {
+app.controller('MainCtrl', ['$http', '$scope', function($http, $scope) {
     var self = this;
-    self.data = [];
-    self.forecast = [];
+    self.day = [];
+    self.forecast =[];
+    var weekDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    	
+    var calcStatus = function(data) {
+    	var status = ["excellent", "good", "fair", "bad", "horrible"]
+    	return status[1];
+    }
 
-    //tes update
 
-
+    // ----------------------------------------------
+    // get current day weather
     $http.get('http://api.openweathermap.org/data/2.5/weather?lat=27.9710&lon=-82.4650&units=imperial', {
-        apiKey: '410046c4d0a1b7158297e8bc5957082f'
-    }).success(function(request) {
-
-
-        var data = request;
-        self.data.city = data.name;
-        self.data.sunset = data.sys.sunset;
-        self.data.temp = data.main.temp;
-        self.data.wind = data.wind.speed;
-        self.data.humidity = data.main.humidity;
-        self.data.cloud = data.clouds;
-
-        console.log(data);
-    });
-
-    $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=27.9710&lon=-82.4650&cnt=10&mode=json&units=imperial', {
         apiKey: '410046c4d0a1b7158297e8bc5957082f'
     }).success(function(response) {
 
-     console.log(response);
+    	// format data
+    	var time = new Date(response.sys.sunset * 1000)
+        var hours = time.getHours() - 12
+        var minutes = time.getMinutes()
 
-        for (i=0; i<7; i++) {
-        	self.forecast[i] = {};
-        	self.forecast[i].hi = response.list[i].temp.max;
-        	self.forecast[i].low = response.list[i].temp.min;
-        	self.forecast[i].icon = response.list[i].weather[0].icon;
-        	self.forecast[i].description = response.list[i].weather[0].description;
-        	self.forecast[i].date = response.list[i].dt;
-    		
-    		
-        }
-      
-
+    	// assign data 
+        self.day.city = response.name.toLowerCase();
+        self.day.sunset = hours + ":" + minutes + "pm";
+        self.day.temp = Math.floor(response.main.temp) + "F";
+        self.day.wind = "wind: " + response.wind.speed + " mph";
+        self.day.humidity = "humidity: " + response.main.humidity + "%";
+        self.day.cloud = "clouds: " + response.weather[0].description;
+        self.day.icon = response.weather[0].icon;
+        self.day.status = calcStatus();
+        
+        
     });
-    
-// var now = new Date();
-// alert(Math.round(new Date().getTime()/1000.0));
 
-var myDate = new Date(1425414194 * 1000);
-console.log(myDate.getDay());
+    // ----------------------------------------------
+	// get 10 day forcast
+    $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=27.9710&lon=-82.4650&cnt=10&mode=json&units=imperial', {
+    apiKey: '410046c4d0a1b7158297e8bc5957082f'
+	    }).success(function(response) {
+		    for (i=0; i<7; i++) {
+
+		    	// format data
+		    	var day = new Date(response.list[i].dt * 1000)
+		    
+		    	// assign data
+		    	self.forecast[i] = {};
+		    	self.forecast[i].day = weekDays[day.getDay()];
+		    	self.forecast[i].hi = Math.floor(response.list[i].temp.max) + "F";
+		    	self.forecast[i].low = Math.floor(response.list[i].temp.min) + "F";
+		    	self.forecast[i].icon = response.list[i].weather[0].icon;	
+		    	self.forecast[i].status = calcStatus();	    	
+		    };
+	});
 
 }]);
 
 
 
-// app.controller('MainCtrl', [function(){
-// 	var self = this;
 
-
-	// // testing data
-	// self.status = "excellent";
-	// self.data = {
-	// 	"city": "houston",
-	// 	"sunset": "6:32",
-	// 	"temp": "74F",
-	// 	"wind": "wind: 23mph",
-	// 	"humidity": "humidity: 10%",
-	// 	"percipitation": "percipitation: 4%",
-	// 	"visibility": "visibility: 7miles",
-	// 	"cloud": "cloud cover: 26%",
-	// }
-
-	// self.forcast = [
-	// 	{
-	// 		"day": "mon",
-	// 		"icon": "statusIcon.png",
-	// 		"low": "50F",
-	// 		"hi": "80F"
-	// 	},
-	// 	{
-	// 		"day": "tue",
-	// 		"icon": "statusIcon.png",
-	// 		"low": "50F",
-	// 		"hi": "80F"
-	// 	},
-	// 	{
-	// 		"day": "wed",
-	// 		"icon": "statusIcon.png",
-	// 		"low": "50F",
-	// 		"hi": "80F"
-	// 	},
-	// 	{
-	// 		"day": "thu",
-	// 		"icon": "statusIcon.png",
-	// 		"low": "50F",
-	// 		"hi": "80F"
-	// 	},
-	// 	{
-	// 		"day": "fri",
-	// 		"icon": "statusIcon.png",
-	// 		"low": "50F",
-	// 		"hi": "80F"
-	// 	},
-	// 	{
-	// 		"day": "sat",
-	// 		"icon": "statusIcon.png",
-	// 		"low": "50F",
-	// 		"hi": "80F"
-	// 	},
-	// 	{
-	// 		"day": "sun",
-	// 		"icon": "statusIcon.png",
-	// 		"low": "50F",
-	// 		"hi": "80F"
-	// 	},
-	// ]
-
-
-}])
 
