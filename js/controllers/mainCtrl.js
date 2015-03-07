9adb995d049150247f71d1554be7492e599acf11
Drew Hamilton <DrewRHamilton@gmail.com>
2015-03-06 22:23:37 -0500

    Google City  Autocomplete is finished in masterctrl.

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,11 +1,11 @@
 app.controller('MainCtrl', ['$http', '$scope', function($http, $scope) {
     var self = this;
     self.day = [];
-    self.forecast =[];
+    self.forecast = [];
     var weekDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-    	
+
     var calcStatus = function(data) {
-    	var status = ["excellent", "good", "fair", "poor", "bad"]
+        var status = ["excellent", "good", "fair", "poor", "bad"]
         var i = 0
         var conditions = []
 
@@ -16,8 +16,8 @@ app.controller('MainCtrl', ['$http', '$scope', function($http, $scope) {
             conditions.humidity = data.main.humidity
         } else if (data.weather[0].id) {
             id = data.weather[0].id
-            // conditions.wind = 
-            conditions.humidity = data.humidity 
+                // conditions.wind = 
+            conditions.humidity = data.humidity
         }
 
         console.log(conditions.humidity)
@@ -45,7 +45,7 @@ app.controller('MainCtrl', ['$http', '$scope', function($http, $scope) {
         }
 
         i = Math.min(Math.max(parseInt(i), 0), 4);
-    	return status[i];
+        return status[i];
     }
 
 
@@ -54,12 +54,12 @@ app.controller('MainCtrl', ['$http', '$scope', function($http, $scope) {
     $http.get('http://api.openweathermap.org/data/2.5/weather?lat=27.9710&lon=-82.4650&units=imperial', {
         apiKey: '410046c4d0a1b7158297e8bc5957082f'
     }).success(function(response) {
-    	// format data
-    	var time = new Date(response.sys.sunset * 1000)
+        // format data
+        var time = new Date(response.sys.sunset * 1000)
         var hours = time.getHours() - 12
         var minutes = time.getMinutes()
 
-    	// assign data 
+        // assign data 
         self.day.city = response.name.toLowerCase();
         self.day.sunset = hours + ":" + minutes + "pm";
         self.day.temp = Math.floor(response.main.temp) + "F";
@@ -68,35 +68,50 @@ app.controller('MainCtrl', ['$http', '$scope', function($http, $scope) {
         self.day.cloud = "clouds: " + response.weather[0].description;
         self.day.icon = response.weather[0].icon;
         self.day.status = calcStatus(response);
-        
-        
+
+
     });
 
     // ----------------------------------------------
-	// get 10 day forcast
+    // get 10 day forcast
     $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=27.9710&lon=-82.4650&cnt=10&mode=json&units=imperial', {
-    apiKey: '410046c4d0a1b7158297e8bc5957082f'
-	    }).success(function(response) {
+        apiKey: '410046c4d0a1b7158297e8bc5957082f'
+    }).success(function(response) {
 
-		    for (i=0; i<7; i++) {
+        for (i = 0; i < 7; i++) {
 
-		    	// format data
-		    	var day = new Date(response.list[i].dt * 1000)
-		    
-		    	// assign data
-		    	self.forecast[i] = {};
-		    	self.forecast[i].day = weekDays[day.getDay()];
-		    	self.forecast[i].hi = Math.floor(response.list[i].temp.max) + "F";
-		    	self.forecast[i].low = Math.floor(response.list[i].temp.min) + "F";
-		    	self.forecast[i].icon = response.list[i].weather[0].icon;	
-		    	self.forecast[i].status = calcStatus(response.list[i]);
+            // format data
+            var day = new Date(response.list[i].dt * 1000)
 
-		    };
-	});
+            // assign data
+            self.forecast[i] = {};
+            self.forecast[i].day = weekDays[day.getDay()];
+            self.forecast[i].hi = Math.floor(response.list[i].temp.max) + "F";
+            self.forecast[i].low = Math.floor(response.list[i].temp.min) + "F";
+            self.forecast[i].icon = response.list[i].weather[0].icon;
+            self.forecast[i].status = calcStatus(response.list[i]);
+
+        };
+    });
+
+
+
+    //--------------------------------------------------
+    //  City Autocomplete from Google 
+
 
 }]);
 
+function initialize() {
+    var input = /** @type {HTMLInputElement} */ (
+        document.getElementById('place'));
+
+    var autocomplete = new google.maps.places.Autocomplete(input, {});
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-
+    // End of Autocomplete
+    //-------------------------------------------
